9d6ea8d60de3a0c2ff0bb6dce3c6f05f0c06f225
chishakitakashi <e7axu4shai@icloud.com>
2022-04-03 18:31:58 +0900

    [□と○]を学習しました。

--- 03_サンプル/01_□と○/main.js
+++ 03_サンプル/01_□と○/main.js
@@ -1,5 +1,14 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function () {
-  figure.classList.toggle("rounded")
+  if (figure.classList.contains("rounded")) {
+    figure.classList.remove("rounded")
+    figure.classList.remove("square")
+    figure.classList.add("triangle")
+  } else if (figure.classList.contains("triangle")) {
+    figure.classList.remove("triangle")
+    figure.classList.add("square")
+  } else {
+    figure.classList.add("rounded")
+  }
 }
