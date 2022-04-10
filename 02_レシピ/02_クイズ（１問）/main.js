f5014298c17f92c0c25607d836b76c0cd4ccda6b
chishakitakashi <e7axu4shai@icloud.com>
2022-04-10 21:26:27 +0900

    [クイズ (1問)発展２]を学習しました。

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,7 +1,7 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
 const feedback = document.getElementById("feedback")
-const container = document.getElementsByClassName("container")
+const container = document.getElementById("container")
 
 // クイズの内容
 const quiz = {
@@ -42,52 +42,50 @@ reloadQuiz()
 // choiceNumber番目の選択肢を選択
 
 // 共通の処理：テキストからカードを作成する
-const createCard = function () {
-  // ボタンを作る
-  const card1 = document.createElement("button")
-  const card2 = document.createElement("button")
-  const card3 = document.createElement("button")
-  const card4 = document.createElement("button")
-  // IDを付与
-  card1.Id = "card1"
-  card2.Id = "card2"
-  card3.Id = "card3"
-  card4.Id = "card4"
-  // container内に作る
-  container.append(card1)
-  container.append(card2)
-  container.append(card3)
-  container.append(card4)
-  // 選択肢（ボタン）の中身を表示
-  card1.textContent = quiz.choices[0].text
-  card2.textContent = quiz.choices[1].text
-  card3.textContent = quiz.choices[2].text
-  card4.textContent = quiz.choices[3].text
+// ボタンを作る
+const card1 = document.createElement("button")
+const card2 = document.createElement("button")
+const card3 = document.createElement("button")
+const card4 = document.createElement("button")
+// IDを付与
+card1.Id = "card1"
+card2.Id = "card2"
+card3.Id = "card3"
+card4.Id = "card4"
+// container内に作る
+container.append(card1)
+container.append(card2)
+container.append(card3)
+container.append(card4)
+// 選択肢（ボタン）の中身を表示
+card1.textContent = quiz.choices[0].text
+card2.textContent = quiz.choices[1].text
+card3.textContent = quiz.choices[2].text
+card4.textContent = quiz.choices[3].text
 
-  card1.onclick = function () {
-    // 0 番目の選択肢を選択
-    choose(0)
-  }
-  card2.onclick = function () {
-    // 1 番目の選択肢を選択
-    choose(1)
-  }
-  card3.onclick = function () {
-    // 2 番目の選択肢を選択
-    choose(2)
-  }
-  card4.onclick = function () {
-    // 3 番目の選択肢を選択
-    choose(3)
-  }
-
-  const choose = function (choiceNumber) {
-    // フィードバックを表示
-    feedback.textContent = quiz.choices[choiceNumber].feedback
-  }
-
-  // return card1.card2.card3.card4をするとエラーぽくなる
-  return container
+card1.onclick = function () {
+  // 0 番目の選択肢を選択
+  choose(0)
+}
+card2.onclick = function () {
+  // 1 番目の選択肢を選択
+  choose(1)
+}
+card3.onclick = function () {
+  // 2 番目の選択肢を選択
+  choose(2)
+}
+card4.onclick = function () {
+  // 3 番目の選択肢を選択
+  choose(3)
 }
 
-createCard()
+const choose = function (choiceNumber) {
+  // フィードバックを表示
+  feedback.textContent = quiz.choices[choiceNumber].feedback
+}
+
+// return card1.card2.card3.card4をするとエラーぽくなる
+//return container
+
+//createCard()
