45e8222aa32771df3b6f26b646c0eb0275343944
chishakitakashi <e7axu4shai@icloud.com>
2022-04-10 20:37:37 +0900

    ゼミでクイズを作りました。

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,27 +1,28 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
 const feedback = document.getElementById("feedback")
+const container = document.getElementsByClassName("container")
 
 // クイズの内容
 const quiz = {
-  text: "この星の名前は何でしょう？",
-  image: "Ganymede.jpg",
+  text: "このラーメンの種類はなんでしょう？",
+  image: "ramen.jpg",
   choices: [
     {
-      text: "ゴリアテ",
-      feedback:
-        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      text: "しょうゆ",
+      feedback: "残念！しょうゆ味じゃないよ",
     },
     {
-      text: "ゼニガメ",
-      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      text: "みそ",
+      feedback: "正解！味噌味だよ！",
     },
     {
-      text: "ガニメデ",
-      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      text: "とんこつ",
+      feedback: "残念！とんこつ味じゃないよ！",
+    },
+    {
+      text: "二郎",
+      feedback: "残念！二郎じゃないよ！",
     },
   ],
 }
@@ -33,31 +34,60 @@ const reloadQuiz = function () {
 
   // 画像を表示
   quizImage.src = "./images/" + quiz.image
-
-  // 選択肢（ボタン）の中身を表示
-  choice1.textContent = quiz.choices[0].text
-  choice2.textContent = quiz.choices[1].text
-  choice3.textContent = quiz.choices[2].text
-}
-
-// choiceNumber番目の選択肢を選択
-const choose = function (choiceNumber) {
-  // フィードバックを表示
-  feedback.textContent = quiz.choices[choiceNumber].feedback
-}
-
-choice1.onclick = function () {
-  // 0 番目の選択肢を選択
-  choose(0)
-}
-choice2.onclick = function () {
-  // 1 番目の選択肢を選択
-  choose(1)
-}
-choice3.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(2)
 }
 
 // reloadQuiz関数 を実行して、クイズを画面に表示する
 reloadQuiz()
+
+// choiceNumber番目の選択肢を選択
+
+// 共通の処理：テキストからカードを作成する
+const createCard = function () {
+  // ボタンを作る
+  const card1 = document.createElement("button")
+  const card2 = document.createElement("button")
+  const card3 = document.createElement("button")
+  const card4 = document.createElement("button")
+  // IDを付与
+  card1.Id = "card1"
+  card2.Id = "card2"
+  card3.Id = "card3"
+  card4.Id = "card4"
+  // container内に作る
+  container.append(card1)
+  container.append(card2)
+  container.append(card3)
+  container.append(card4)
+  // 選択肢（ボタン）の中身を表示
+  card1.textContent = quiz.choices[0].text
+  card2.textContent = quiz.choices[1].text
+  card3.textContent = quiz.choices[2].text
+  card4.textContent = quiz.choices[3].text
+
+  card1.onclick = function () {
+    // 0 番目の選択肢を選択
+    choose(0)
+  }
+  card2.onclick = function () {
+    // 1 番目の選択肢を選択
+    choose(1)
+  }
+  card3.onclick = function () {
+    // 2 番目の選択肢を選択
+    choose(2)
+  }
+  card4.onclick = function () {
+    // 3 番目の選択肢を選択
+    choose(3)
+  }
+
+  const choose = function (choiceNumber) {
+    // フィードバックを表示
+    feedback.textContent = quiz.choices[choiceNumber].feedback
+  }
+
+  // return card1.card2.card3.card4をするとエラーぽくなる
+  return container
+}
+
+createCard()
